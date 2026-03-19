995be8ef011d5ccaa75258853c313f0aef2cb6a6
MATSUMOTO, Katsuyoshi <github@katsyoshi.org>
2026-03-19 11:41:24 +0900

    Site updated at 2026-03-19 02:41:24 UTC

--- load_sum.rb
+++
@@ -1,7 +0,0 @@
-require "fiddle/import"
-
-module LoadSum
-  extend Fiddle::Importer
-  dlload "./libfibo.so"
-  extern "int sum(int, int)"
-end
--- move.rb
+++
@@ -1,5 +0,0 @@
-require 'fileutils'
-Dir.glob("_posts/*.markdown").each do |file|
-  new_file = file.sub(/\.markdown/, ".md")
-  FileUtils.mv(file, new_file)
-end
